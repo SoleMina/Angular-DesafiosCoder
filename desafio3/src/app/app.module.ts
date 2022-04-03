a9511707fb02e23872d69228d579bc4cf7e64900
SoleMina <karina.pradogutierrez@gmail.com>
2022-04-02 19:17:21 -0500

    update

--- desafio3/src/app/app.module.ts
+++ desafio3/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContenidoComponent } from './components/contenido/contenido.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContenidoComponent,
+    FormularioComponent,
+    NavbarComponent,
+    ToolbarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
