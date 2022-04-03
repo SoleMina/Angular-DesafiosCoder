d470e6d8359c87c3531dd7b051fe2a47acfa2e06
SoleMina <karina.pradogutierrez@gmail.com>
2022-04-02 20:10:32 -0500

    update

--- desafio3/src/app/components/contenido/contenido.component.ts
+++ desafio3/src/app/components/contenido/contenido.component.ts
@@ -3,13 +3,51 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contenido',
   templateUrl: './contenido.component.html',
-  styleUrls: ['./contenido.component.css']
+  styleUrls: ['./contenido.component.css'],
 })
 export class ContenidoComponent implements OnInit {
+  title: string = 'List of students';
+  fecha: any = Date.now();
+  alumnos: any[] = [
+    {
+      id: 1,
+      name: 'Karina',
+      lastname: 'Prado',
+      grade: 18,
+      email: 'karina@gmail.com',
+      curso: 'angular',
+    },
+    {
+      id: 2,
+      name: 'Carla',
+      lastname: 'Caceres',
+      grade: 19,
+      email: 'carla2022@gmail.com',
+      curso: 'angular',
+    },
+    {
+      id: 3,
+      name: 'Samantha',
+      lastname: 'Paredes',
+      grade: 11,
+      email: 'sam.paredes@gmail.com',
+      curso: 'angular',
+    },
+    {
+      id: 4,
+      name: 'Estefania',
+      lastname: 'Castro',
+      grade: 12,
+      email: 'castro.estefania@gmail.com',
+      curso: 'angular',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  isOven() {}
+  userClicked(username: string) {
+    console.log('El usuario ' + username + ' fue clickeado');
   }
-
 }
