26d126dd2b006c20fb2d3c79c345755087bc68a6
SoleMina <karina.pradogutierrez@gmail.com>
2022-04-12 12:46:39 -0500

    update

--- desafio4/src/app/pipes/fullname.pipe.ts
+++ desafio4/src/app/pipes/fullname.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { AlumnoService } from '../services/alumno.service';
 
 @Pipe({
-  name: 'fullname'
+  name: 'fullname',
 })
 export class FullnamePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  alumnos: any[] = [];
+  transform(value: any): string {
+    let resultado: string;
+    //console.log(value);
+    //console.log(this.alumnoService.obtenerAlumnos());
+    //console.log(this.alumnoService.obtenerAlumnos()[value].name);
+    resultado =
+      this.alumnoService.obtenerAlumnos()[value - 1].name +
+      ' ' +
+      this.alumnoService.obtenerAlumnos()[value - 1].age;
+    return resultado;
   }
-
+  constructor(private alumnoService: AlumnoService) {}
 }
