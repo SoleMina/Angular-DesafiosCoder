0b27d29da7caa199983e6a2fcd53f70ca8c6dac7
SoleMina <karina.pradogutierrez@gmail.com>
2022-05-04 16:19:09 -0500

    update

---
+++ desafio4/src/app/autenticacion/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    let sesion: any = JSON.parse(localStorage.getItem('sesion') || '{}');
+
+    if (sesion.activa) {
+      return true;
+    } else {
+      this.router.navigate(['autenticacion', 'login']);
+      return false;
+    }
+    //
+    return sesion.activa;
+  }
+}
